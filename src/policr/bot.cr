6bb5d6dc9b1d03d69f7e187bdf61aad35723d1bd
Hentioe_Cl <me@bluerain.io>
2019-03-30 11:33:26 +0800

    Add unban InlineKeyboardButton

--- src/policr/bot.cr
+++ src/policr/bot.cr
@@ -16,6 +16,28 @@ module Policr
       end
     end
 
+    def handle(query : TelegramBot::CallbackQuery)
+      if message = query.message
+        chat_id = message.chat.id
+        from_user_id = query.from.id
+        member = get_chat_member(chat_id, from_user_id)
+
+        if (user_id = query.data) && (member.status == "creator" || member.status == "admin")
+          begin
+            unban_r = unban_chat_member(chat_id, user_id.to_i)
+            edit_message_text(chat_id: chat_id, message_id: message.message_id,
+              text: "已经被解封了，快通知他回来。", reply_markup: nil) if unban_r
+          rescue e : TelegramBot::APIException
+            reason = "原因不明"
+            if data = e.data
+              reason = data["description"]
+            end
+            answer_callback_query(query.id, text: "解封失败，#{reason}", show_alert: true)
+          end
+        end
+      end
+    end
+
     def handle(msg : TelegramBot::Message)
       new_members = msg.new_chat_members
       new_members.each do |member|
@@ -35,8 +57,12 @@ module Policr
           edit_message_text(chat_id: sended_msg.chat.id, message_id: sended_msg.message_id,
             text: "(ﾉ>ω<)ﾉ 已成功丢出去一只清真，真棒！") if kick_r
         rescue e : TelegramBot::APIException
+          channed_id = msg.chat.id
+          user_id = member.id
+          ikb_list = TelegramBot::InlineKeyboardMarkup.new
+          ikb_list << TelegramBot::InlineKeyboardButton.new(text: "解除封禁", callback_data: user_id.to_s)
           edit_message_text(chat_id: sended_msg.chat.id, message_id: sended_msg.message_id,
-            text: "╰(〒皿〒)╯ 啥情况，这枚清真移除失败了。") unless kick_r
+            text: "╰(〒皿〒)╯ 啥情况，这枚清真移除失败了。", reply_markup: ikb_list) unless kick_r
         end
       end
     end
