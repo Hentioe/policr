82debcc23d5059a1d9d21512ea45990cd7acfe03
Hentioe <me@bluerain.io>
2019-05-18 14:46:16 +0800

    Update website

--- src/policr/web.cr
+++ src/policr/web.cr
@@ -13,6 +13,11 @@ module Policr::Web
       render "src/views/index.html.ecr", "src/views/layout.html.ecr"
     end
 
+    get "/guide" do
+      title = "使用指南"
+      render "src/views/guide.html.ecr", "src/views/layout.html.ecr"
+    end
+
     get "/admin" do
       title = "后台管理"
       render "src/views/admin.html.ecr", "src/views/layout.html.ecr"
