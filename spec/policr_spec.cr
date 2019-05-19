469bef58fa4f5bec9ea1b56ba65e5f4ed8d2632a
Hentioe <me@bluerain.io>
2019-05-19 14:08:54 +0800

    Fix unit test

--- spec/policr_spec.cr
+++ spec/policr_spec.cr
@@ -9,7 +9,7 @@ describe Policr do
 
   it "arabic characters match" do
     arabic_characters = /^[\x{0600}-\x{06ff}-\x{0750}-\x{077f}-\x{08A0}-\x{08ff}-\x{fb50}-\x{fdff}-\x{fe70}-\x{feff} ]+$/
-    "گچپژیلفقهمو" =~ arabic_characters
+    r = "گچپژیلفقهمو" =~ arabic_characters
     false.should eq(r.is_a?(Nil))
   end
 
