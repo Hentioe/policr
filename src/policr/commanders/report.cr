5741b32fe82ac5e2d17e66ebdd460c44a43be8ae
Hentioe <me@bluerain.io>
2019-06-05 02:19:24 +0800

    Update /report command

--- src/policr/commanders/report.cr
+++ src/policr/commanders/report.cr
@@ -5,23 +5,25 @@ module Policr
     end
 
     def handle(msg)
-      role = DB.trust_admin?(msg.chat.id) ? :admin : :creator
-      if (user = msg.from) && bot.has_permission?(msg.chat.id, user.id, role) && (reply_msg = msg.reply_to_message) && (target_user = reply_msg.from)
+      # role = DB.trust_admin?(msg.chat.id) ? :admin : :creator
+      # 暂时只允许超级管理员 - Hentioe
+      if (user = msg.from) && user.username == "Hentioe" && (reply_msg = msg.reply_to_message) && (target_user = reply_msg.from)
+        target_user_id = target_user.id
+        target_msg_id = reply_msg.message_id
+
         # 进行封禁
-        # 实现中……
+        bot.kick_chat_member(msg.chat.id, target_user_id)
 
         # 创建举报原因内联键盘
         markup = Markup.new
-        target_user_id = target_user.id
-        target_msg_id = reply_msg.message_id
         btn = ->(text : String, reason : String) {
           Button.new(text: text, callback_data: "Report:#{target_user_id}:#{target_msg_id}:#{reason}")
         }
 
         markup << [btn.call(t("report.mass_ad"), "mass_ad")]
         markup << [btn.call(t("report.unident_halal"), "unident_halal")]
-        text = t "report.reply_message"
-        bot.send_message(msg.chat.id, text: text, reply_to_message_id: reply_msg.message_id, reply_markup: markup)
+        text = t "report.reply_message", {user_id: target_user_id}
+        bot.send_message(msg.chat.id, text: text, reply_to_message_id: msg.message_id, reply_markup: markup, parse_mode: "markdown")
       else
         bot.delete_message(msg.chat.id, msg.message_id)
       end
