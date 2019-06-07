9f9f7c69276e7fa7459777cdc0e383c9ddfda6f6
Hentioe_Cl <me@bluerain.io>
2019-06-07 20:20:49 +0800

    Fix #36

--- src/policr/handlers/halal_message.cr
+++ src/policr/handlers/halal_message.cr
@@ -1,9 +1,9 @@
 module Policr
   class HalalMessageHandler < Handler
-    ARABIC_CHARACTERS = /^[\x{0600}-\x{06FF}-\x{0750}-\x{077F}-\x{08A0}-\x{08FF}-\x{FB50}-\x{FDFF}-\x{FE70}-\x{FEFF}-\x{10E60}-\x{10E7F}-\x{1EC70}-\x{1ECBF}-\x{1ED00}-\x{1ED4F}-\x{1EE00}-\x{1EEFF} ]+$/
+    ARABIC_CHARACTERS = /^[\x{0600}-\x{06FF}\x{0750}-\x{077F}\x{08A0}-\x{08FF}\x{FB50}-\x{FDFF}\x{FE70}-\x{FEFF}\x{10E60}-\x{10E7F}\x{1EC70}-\x{1ECBF}\x{1ED00}-\x{1ED4F}\x{1EE00}-\x{1EEFF} ]+$/
     SAFE_MSG_SIZE     = 6 # 消息的安全长度
 
-    ARABIC_CHARACTER = /[\x{0600}-\x{06FF}-\x{0750}-\x{077F}-\x{08A0}-\x{08FF}-\x{FB50}-\x{FDFF}-\x{FE70}-\x{FEFF}-\x{10E60}-\x{10E7F}-\x{1EC70}-\x{1ECBF}-\x{1ED00}-\x{1ED4F}-\x{1EE00}-\x{1EEFF}]/
+    ARABIC_CHARACTER = /[\x{0600}-\x{06FF}\x{0750}-\x{077F}\x{08A0}-\x{08FF}\x{FB50}-\x{FDFF}\x{FE70}-\x{FEFF}\x{10E60}-\x{10E7F}\x{1EC70}-\x{1ECBF}\x{1ED00}-\x{1ED4F}\x{1EE00}-\x{1EEFF}]/
 
     def match(msg)
       all_pass? [
