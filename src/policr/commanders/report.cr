c7ce6424ae746836e84c13473e73ec5c91c555d1
Hentioe <me@bluerain.io>
2019-07-02 19:52:55 +0800

    Fix #58

--- src/policr/commanders/report.cr
+++ src/policr/commanders/report.cr
@@ -10,6 +10,23 @@ module Policr
         target_user_id = target_user.id
         target_msg_id = reply_msg.message_id
 
+        error_msg =
+          if author_id == target_user_id # 不能举报自己
+            t "report.author_cant_self"
+          elsif target_user_id == bot.self_id # 不能举报本机器人
+            t "report.target_user_cant_the_bot"
+          elsif target_user_id == 777000 # 举报目标用户无效
+            t "report.target_user_invalid"
+          end
+        if error_msg
+          bot.send_message(
+            msg.chat.id, text: error_msg,
+            reply_to_message_id: msg.message_id,
+            parse_mode: "markdown"
+          )
+          return
+        end
+
         # 创建举报原因内联键盘
         markup = Markup.new
         btn = ->(text : String, reason : ReportReason) {
