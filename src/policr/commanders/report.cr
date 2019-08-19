a668dede6bcbf4f803de3f789b08654e8b612735
Hentioe <me@bluerain.io>
2019-08-19 08:28:58 +0000

    Fix #154

--- src/policr/commanders/report.cr
+++ src/policr/commanders/report.cr
@@ -16,37 +16,47 @@ module Policr
         target_user_id = target_user.id
         target_msg_id = reply_msg.message_id
         chat_id = msg.chat.id
+        msg_id = msg.message_id
 
-        if error_msg = check_legality(author_id, target_user_id)
-          bot.send_message chat_id, error_msg, reply_to_message_id: msg.message_id
-          return
+        sended_msg =
+          if error_msg = check_legality(author_id, target_user_id)
+            bot.send_message chat_id, error_msg, reply_to_message_id: msg_id
+          else
+            # 创建举报原因内联键盘
+            markup = Markup.new
+            btn = ->(text : String, reason : ReportReason) {
+              data = "Report:#{author_id}:#{target_user_id}:#{target_msg_id}:#{reason.value}"
+              Button.new(text: text, callback_data: data)
+            }
+
+            if ReportCommander.is_file? reply_msg
+              markup << [btn.call(t("report.virus_file"), Reason::VirusFile)]
+              markup << [btn.call(t("report.promo_file"), Reason::PromoFile)]
+            else
+              markup << [btn.call(t("report.mass_ad"), Reason::MassAd)]
+              markup << [btn.call(t("report.halal"), Reason::Halal)]
+              markup << [btn.call(t("report.hateful"), Reason::Hateful)]
+              markup << [btn.call(t("report.adname"), Reason::Adname)]
+            end
+
+            text = t "report.admin_reply", {user_id: target_user_id, voting_channel: bot.voting_channel}
+            # 缓存被举报用户
+            Cache.carving_report_target_msg chat_id, target_msg_id, target_user
+            bot.send_message(
+              msg.chat.id,
+              text: text,
+              reply_to_message_id: msg_id,
+              reply_markup: markup
+            )
+          end
+        if sended_msg
+          # 清理完成的举报
+          _del_msg_id = sended_msg.message_id
+          Model::CleanMode.working chat_id, CleanDeleteTarget::Report do
+            spawn bot.delete_message chat_id, _del_msg_id
+            spawn bot.delete_message chat_id, msg_id
+          end
         end
-
-        # 创建举报原因内联键盘
-        markup = Markup.new
-        btn = ->(text : String, reason : ReportReason) {
-          Button.new(text: text, callback_data: "Report:#{author_id}:#{target_user_id}:#{target_msg_id}:#{reason.value}")
-        }
-
-        if ReportCommander.is_file? reply_msg
-          markup << [btn.call(t("report.virus_file"), Reason::VirusFile)]
-          markup << [btn.call(t("report.promo_file"), Reason::PromoFile)]
-        else
-          markup << [btn.call(t("report.mass_ad"), Reason::MassAd)]
-          markup << [btn.call(t("report.halal"), Reason::Halal)]
-          markup << [btn.call(t("report.hateful"), Reason::Hateful)]
-          markup << [btn.call(t("report.adname"), Reason::Adname)]
-        end
-
-        text = t "report.admin_reply", {user_id: target_user_id, voting_channel: bot.voting_channel}
-        bot.send_message(
-          msg.chat.id,
-          text: text,
-          reply_to_message_id: msg.message_id,
-          reply_markup: markup
-        )
-        # 缓存被举报用户
-        Cache.carving_report_target_msg chat_id, target_msg_id, target_user
       else
         bot.delete_message(msg.chat.id, msg.message_id)
       end
@@ -60,6 +70,9 @@ module Policr
       elsif target_user_id == 777000 # 举报目标用户无效
         t "report.target_user_invalid"
       end
+    end
+
+    def repeat?(chat_id, msg_id)
     end
   end
 end
