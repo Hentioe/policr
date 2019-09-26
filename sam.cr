dcd4bf324a2853052aa09b8e75cb23de9c6200e6
CUILEI <me@bluerain.io>
2019-09-26 08:50:27 +0800

    Add clear block content tasks

--- sam.cr
+++ sam.cr
@@ -19,24 +19,29 @@ namespace "rocksdb" do
   end
 
   namespace "migrate" do
-    desc "迁移清真白名单"
-    task "halal_white" do
-      prefix = "halal_white"
-      puts "#{prefix} started."
-      query_by_prefix "#{prefix}_" do |key, value|
-        user_id, is_contains =
-          if md = /(\d+)$/.match key
-            {md[1].to_i, value.to_i == 1}
-          else
-            {nil, nil}
-          end
-        if user_id && is_contains
-          Policr::Model::HalalWhiteList.add!(user_id, -1)
-        end
-      end
-      puts "#{prefix} done."
-    end
+    # desc "迁移清真白名单"
+    # task "halal_white" do
+    #   prefix = "halal_white"
+    #   puts "#{prefix} started."
+    #   query_by_prefix "#{prefix}_" do |key, value|
+    #     user_id, is_contains =
+    #       if md = /(\d+)$/.match key
+    #         {md[1].to_i, value.to_i == 1}
+    #       else
+    #         {nil, nil}
+    #       end
+    #     if user_id && is_contains
+    #       Policr::Model::HalalWhiteList.add!(user_id, -1)
+    #     end
+    #   end
+    #   puts "#{prefix} done."
+    # end
   end
+end
+
+desc "清空内容屏蔽规则"
+task "clear_block_contents" do
+  Policr::Model::BlockContent.all.delete
 end
 
 def query_by_prefix(prefix)
